b146800f089031558496f883b76606d9727da53b
amir-eager <amyrhsynprwyzy92@gmail.com>
2026-02-06 17:53:12 +0330

    added flake + home-manager setup + fixed warnings

--- flake.nix
+++ flake.nix
@@ -1,31 +1,34 @@
 {
-  description = "Amir's NixOS configuration";
+  description = "Amir's NixOS + Home-Manager config";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    # اگر بعداً unstable خواستی: "github:NixOS/nixpkgs/nixos-unstable"
+    # اگر unstable می‌خوای: "github:NixOS/nixpkgs/nixos-unstable"
 
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";   # هم‌ نسخه با nixpkgs اصلی
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: let
-    system = "x86_64-linux";
-  in {
+  outputs = { self, nixpkgs, home-manager, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      inherit system;
+      system = "x86_64-linux";
       modules = [
         ./hardware-configuration.nix
         ./configuration.nix
 
-        # home-manager module
+        # اضافه کردن home-manager به عنوان module
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+
+          # کاربر amir رو تعریف می‌کنیم
           home-manager.users.amir = import ./home.nix;
+
+          # اختیاری: اجازه بده home-manager از nixpkgs سیستم استفاده کنه
+          home-manager.extraSpecialArgs = { inherit nixpkgs; };
         }
       ];
     };
--- home.nix
+++ home.nix
@@ -1,17 +1,27 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "amir";
-  home.homeDirectory = "/home/amir";
+  # نسخه state برای home-manager
   home.stateVersion = "25.11";
 
-  # اینجا بعداً برنامه‌ها و dotfiles رو اضافه می‌کنیم
+  # username و homeDirectory (اختیاری اگر useUserPackages=true باشه)
+  home.username = "amir";
+  home.homeDirectory = "/home/amir";
+
+  # برنامه‌های نمونه که فقط برای کاربر amir نصب می‌شن
   home.packages = with pkgs; [
-    # مثلاً
     neovim
     git
+    htop
+    # بعداً می‌تونی zsh, tmux, qtile config و ... اضافه کنی
   ];
 
-  # مثلاً zsh رو فعال کنیم
-  programs.zsh.enable = true;
+  # مثلاً zsh رو فعال کنیم (اختیاری)
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+  };
+
+  # اینجا بعداً dotfiles مثل ~/.config/qtile/config.py رو تعریف می‌کنیم
 }
